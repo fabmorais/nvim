fd06757ff83eb1d3dd9407f45f2dfd38666efc8c
Fabio M <fabmorais@pm.me>
2023-03-05 14:02:10 +0100

    enable select all shortcut

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -8,9 +8,9 @@ map("n", "<C-h>", "<C-w>h", { noremap = true, silent = false })
 map("n", "<C-l>", "<C-w>l", { noremap = true, silent = false })
 map("n", "<C-j>", "<C-w>j", { noremap = true, silent = false })
 map("n", "<C-k>", "<C-w>k", { noremap = true, silent = false })
-
 map("v", "<", "<gv", { noremap = true, silent = false })
 map("v", ">", "<gv", { noremap = true, silent = false })
+map("n", "gv", ":norm ggVG <CR>", opts)
 
 -- map(
 --   "n",
