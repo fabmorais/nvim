9ab83fde1be168b0e1ab9f50a908c7ecd3e1bb72
fab <fabmorais@pm.me>
2024-06-07 17:04:19 +0200

    Remap C-d & C-d to C-dzz C-uzz to center the buffer navigation

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -12,6 +12,8 @@ map("v", "<", "<gv", { noremap = true, silent = false })
 map("v", ">", "<gv", { noremap = true, silent = false })
 map("n", "gv", ":norm ggVG <CR>", opts)
 map("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+map("n", "<C-d>", "<C-d>zz", { noremap = true, silent = false })
+map("n", "<C-u>", "<C-u>zz", { noremap = true, silent = false })
 
 -- map(
 --   "n",
