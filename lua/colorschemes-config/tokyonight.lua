31243217a3703fae87801f6dcacb51fa69abbb20
fab <fabmorais@pm.me>
2024-06-14 18:01:00 +0200

    add more colorschemes

---
+++ lua/colorschemes-config/tokyonight.lua
@@ -0,0 +1,6 @@
+require("tokyonight").setup({
+	style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+	transparent = true,
+})
+
+vim.cmd([[colorscheme tokyonight]])
