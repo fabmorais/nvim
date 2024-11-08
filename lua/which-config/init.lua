69296d8f4690886e067f8e17a320ce9278dc51da
fabmorais <fabmorais@pm.me>
2024-11-08 18:20:51 +0100

    Refactor which-key

--- lua/which-config/init.lua
+++ lua/which-config/init.lua
@@ -1,20 +1,4 @@
 local wk = require("which-key")
-wk.setup({
-	plugins = {
-		marks = false,
-		registers = false,
-		spelling = { enabled = false, suggestions = 20 },
-		presets = {
-			operators = false,
-			motions = false,
-			text_objects = false,
-			windows = false,
-			nav = false,
-			z = false,
-			g = false,
-		},
-	},
-})
 
 local Terminal = require("toggleterm.terminal").Terminal
 
@@ -28,108 +12,233 @@ local toggle_lazygit = function()
 	return lazygit:toggle()
 end
 
-local mappings = {
-	["/"] = { ":CommentToggle<cr>", "Toggle Comment" },
-	c = {
-		name = "Dashboard",
-		c = { ":Dashboard<cr>", "Open dashboard" },
+wk.add({
+	-- General
+	{
+		mode = { "n", "v" }, -- NORMAL and VISUAL mode
+		{ "<leader>q", "<cmd>q<cr>", desc = "Quit" },
+		{ "<leader>w", "<cmd>w<cr>", desc = "Write" },
 	},
-	q = { ":q<cr>", "Quit" },
-	Q = { ":wq<cr>", "Save & Quit" },
-	w = { ":w<cr>", "Save" },
-	x = { ":bdelete<cr>", "Close" },
-	E = { ":e ~/.config/nvim/init.lua<cr>", "Edit config" },
-	f = {
-		name = "Telescope",
-		f = { ":Telescope find_files<cr>", "Telescope find files" },
-		g = { ":Telescope live_grep<cr>", "Telescope live grep" },
-		r = { ":Telescope grep_string<cr>", "Telescope grep string" },
-		o = { ":Telescope oldfiles<cr>", "Telescope find old files" },
-		h = { ":Telescope help_tags<cr>", "Telescope help page" },
-		b = { ":Telescope buffers<cr>", "Telescope find buffers" },
-		m = { ":Telescope marks<CR>", "telescope find marks" },
-		z = { ":Telescope current_buffer_fuzzy_find<CR>", "telescope find in current buffer" },
-		c = { ":Telescope git_commits<CR>", "telescope git commits" },
-		s = { ":Telescope git_status<CR>", "telescope git status" },
+	-- Dashboard
+	{ "<leader>d", group = "Dashboard" }, -- group
+	{
+		"<leader>db",
+		":Dashboard<cr>",
+		desc = "Open dashboard",
+		mode = "n",
 	},
-	g = {
-		name = "ChatGPT",
-		c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-		b = { "<cmd>ChatGPTCompleteCode<CR>", "ChatGPTCompleteCode" },
-		e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-		g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-		t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-		k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-		d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-		a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-		o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-		s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-		f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-		x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-		r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-		l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+	-- Terminal
+	{ "<leader>t", group = "Terminal" }, -- group
+	{
+		"<leader>tt",
+		"<cmd>ToggleTerm direction=horizontal<cr>",
+		desc = "Split Below",
+		mode = "n",
 	},
-	b = {
-		name = "Telescope Bookmarks",
-		m = { ":Telescope marks<cr>", "Bookmarks" },
+	{
+		"<leader>tf",
+		toggle_float,
+		desc = "Floating Terminal",
+		mode = "n",
 	},
-	t = {
-		name = "Terminal",
-		t = { ":ToggleTerm<cr>", "Split Below" },
-		f = { toggle_float, "Floating Terminal" },
-		l = { toggle_lazygit, "LazyGit" },
+	{
+		"<leader>tl",
+		toggle_lazygit,
+		desc = "LazyGit",
+		mode = "n",
 	},
-	l = {
-		name = "LSP",
-		i = { ":LspInfo<cr>", "Connected Language Servers" },
-		k = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help" },
-		K = { "<cmd>Lspsaga hover_doc<cr>", "Hover Commands" },
-		w = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>", "Add Workspace Folder" },
-		W = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>", "Remove Workspace Folder" },
-		l = {
-			"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>",
-			"List Workspace Folders",
-		},
-		t = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type Definition" },
-		d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Go To Definition" },
-		D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Go To Declaration" },
-		r = { "<cmd>lua vim.lsp.buf.references()<cr>", "References" },
-		R = { "<cmd>Lspsaga rename<cr>", "Rename" },
-		a = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
-		e = { "<cmd>Lspsaga show_line_diagnostics<cr>", "Show Line Diagnostics" },
-		n = { "<cmd>Lspsaga diagnostic_jump_next<cr>", "Go To Next Diagnostic" },
-		N = { "<cmd>Lspsaga diagnostic_jump_prev<cr>", "Go To Previous Diagnostic" },
+	-- Close Buffer
+	{
+		"<leader>x",
+		"<cmd>bdelete<cr>",
+		desc = "Close window",
+		mode = "n",
 	},
-	p = {
-		name = "Packer",
-		r = { ":PackerClean<cr>", "Remove Unused Plugins" },
-		c = { ":PackerCompile profile=true<cr>", "Recompile Plugins" },
-		i = { ":PackerInstall<cr>", "Install Plugins" },
-		p = { ":PackerProfile<cr>", "Packer Profile" },
-		s = { ":PackerSync<cr>", "Sync Plugins" },
-		S = { ":PackerStatus<cr>", "Packer Status" },
-		u = { ":PackerUpdate<cr>", "Update Plugins" },
+	-- Telescope
+	{ "<leader>f", group = "File" }, -- group
+	{
+		"<leader>ff",
+		"<cmd>Telescope find_files<cr>",
+		desc = "Find File",
+		mode = "n",
 	},
-	-- debugger mappings --
-	d = {
-		name = "Debugger",
-		c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
-		o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
-		i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
-		O = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
-		b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-		s = {
-			"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
-			"Set Breakpoint condition",
-		},
-		B = {
-			"<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>",
-			"Set Breakpoint log message",
-		},
-		n = { "<cmd>lua require'dap'.repl.open()<cr>", "Open" },
-		l = { "<cmd>lua require'dap'.run_last()<cr>", "Run Last" },
+	{
+		"<leader>fg",
+		"<cmd>Telescope live_grep<cr>",
+		desc = "Live Grep",
+		mode = "n",
 	},
-}
+	{
+		"<leader>fb",
+		"<cmd>Telescope buffers<cr>",
+		desc = "Buffers",
+		mode = "n",
+	},
+	{
+		"<leader>fh",
+		"<cmd>Telescope help_tags<cr>",
+		desc = "Help",
+		mode = "n",
+	},
+	{
+		"<leader>fk",
+		"<cmd>Telescope keymaps<cr>",
+		desc = "Keymaps",
+		mode = "n",
+	},
+	{
+		"<leader>fo",
+		"<cmd>Telescope oldfiles<cr>",
+		desc = "Find Recent",
+		mode = "n",
+	},
+	{
+		"<leader>fd",
+		"<cmd>Telescope diagnostics<cr>",
+		desc = "Diagnostics",
+		mode = "n",
+	},
+	{
+		"<leader>fm",
+		"<cmd>Telescope marks<cr>",
+		desc = "Marks",
+		mode = "n",
+	},
+	{
+		"<leader>fr",
+		"<cmd>Telescope grep_string<cr>",
+		desc = "Find Word",
+		mode = "n",
+	},
 
-local opts = { prefix = "<leader>" }
-wk.register(mappings, opts)
+	-- LSP
+	{ "<leader>l", group = "Lsp" }, -- group
+	{
+		"<leader>li",
+		"<cmd>LspInfo<cr>",
+		desc = "Connected Language Servers",
+		mode = "n",
+	},
+	{
+		"<leader>lR",
+		"<cmd>Lspsaga rename<cr>",
+		desc = "Rename",
+		mode = "n",
+	},
+	{
+		"<leader>la",
+		"<cmd>Lspsaga code_action<cr>",
+		desc = "Code Action",
+		mode = "n",
+	},
+	{
+		"<leader>ln",
+		"<cmd>Lspsaga diagnostic_jump_next<cr>",
+		desc = "Go To Next Diagnostic",
+		mode = "n",
+	},
+	{
+		"<leader>lN",
+		"<cmd>Lspsaga diagnostic_jump_prev<cr>",
+		desc = "Go To Previous Diagnostic",
+		mode = "n",
+	},
+	{
+		"<leader>lr",
+		"<cmd>lua vim.lsp.buf.references()<cr>",
+		desc = "References",
+		mode = "n",
+	},
+	{
+		"<leader>le",
+		"<cmd>Lspsaga show_line_diagnostics<cr>",
+		desc = "Show Line Diagnostics",
+		mode = "n",
+	},
+	{
+		"<leader>lt",
+		"<cmd>lua vim.lsp.buf.type_definition()<cr>",
+		desc = "Type Definition",
+		mode = "n",
+	},
+	{
+		"<leader>ld",
+		"<cmd>lua vim.lsp.buf.definition()<cr>",
+		desc = "Go To Definition",
+		mode = "n",
+	},
+	{
+		"<leader>lD",
+		"<cmd>lua vim.lsp.buf.declaration()<cr>",
+		desc = "Go To Declaration",
+		mode = "n",
+	},
+	{
+		"<leader>lk",
+		"<cmd>lua vim.lsp.buf.signature_help()<cr>",
+		desc = "Signature Help",
+		mode = "n",
+	},
+	{
+		"<leader>lh",
+		"<cmd>lua vim.lsp.buf.hover()<cr>",
+		desc = "Hover",
+		mode = "n",
+	},
+	{
+		"<leader>ll",
+		"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>",
+		desc = "List Workspace Folders",
+		mode = "n",
+	},
+	{
+		"<leader>lw",
+		"<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>",
+		desc = "Add Workspace Folder",
+		mode = "n",
+	},
+	{
+		"<leader>lW",
+		"<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>",
+		desc = "Remove Workspace Folder",
+		mode = "n",
+	},
+
+	-- Git
+	{ "<leader>r", group = "Git" }, -- group
+	{
+		"<leader>rc",
+		"<cmd>Telescope git_commits<CR>",
+		desc = "Git commits",
+		mode = "n",
+	},
+	{
+		"<leader>rz",
+		"<cmd>Telescope git_status<CR>",
+		desc = "Git status",
+		mode = "n",
+	},
+	{
+		"<leader>rs",
+		"<cmd>lua require 'gitsigns'.stage_buffer()<cr>",
+		desc = "Stage Buffer",
+		mode = "n",
+	},
+	{ "<leader>rg", "<cmd>lua require 'gitsigns'.status()<cr>", desc = "Status", mode = "n" },
+	{
+		"<leader>rb",
+		"<cmd>lua require 'gitsigns'.blame_line()<cr>",
+		desc = "Blame Line",
+		mode = "n",
+	},
+	{ "<leader>rt", "<cmd>lua require 'gitsigns'.toggle_current_line_blame()<cr>", desc = "Toggle Blame" },
+	{ "<leader>rr", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", desc = "Reset Buffer" },
+	{ "<leader>ru", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", desc = "Undo Stage Hunk" },
+	{ "<leader>rp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", desc = "Preview Hunk" },
+	{ "<leader>rd", "<cmd>lua require 'gitsigns'.diffthis()<cr>", desc = "Diff This" },
+	{ "<leader>rD", "<cmd>lua require 'gitsigns'.diffthis('~')<cr>", desc = "Diff This ~" },
+	{ "<leader>rh", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", desc = "Reset Hunk" },
+	{ "<leader>rH", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", desc = "Reset Buffer" },
+	{ "<leader>rS", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", desc = "Stage Hunk" },
+	{ "<leader>rk", "<cmd>lua require 'gitsigns'.toggle_deleted()<cr>", desc = "Toggle Deleted" },
+})
